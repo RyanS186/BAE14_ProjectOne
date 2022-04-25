e688ccb63c26b1a261ed2e4f05b45ba9f6816bbc
RyanS186 <ryansharp186@gmail.com>
2022-04-25 18:13:13 +0100

    RS: Created getAll and getById methods

--- src/main/java/com/qa/baespring/service/RecordsService.java
+++ src/main/java/com/qa/baespring/service/RecordsService.java
@@ -1,18 +1,30 @@
 package com.qa.baespring.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.qa.baespring.domain.Records;
 import com.qa.baespring.repo.RecordsRepo;
 
 @Service
 public class RecordsService {
 
-	//Link to repo
+	// Link to repo
 	private RecordsRepo repo;
 	
 	public RecordsService(RecordsRepo repo) {
 		this.repo = repo;
 	}
 	
+	// Get ALL records
+	public List<Records> getAll() {
+		return repo.findAll();
+	}
+	
+	// Get by Id
+	public Records getByID(long id) {
+		return repo.findById(id).get();
+	}
 	
 }
