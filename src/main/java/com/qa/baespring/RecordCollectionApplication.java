d2eb317b8f343f50ceb915da3de6f72f2b1db6a1
RyanS186 <ryansharp186@gmail.com>
2022-04-25 11:47:17 +0100

    RS: init commit

---
+++ src/main/java/com/qa/baespring/RecordCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.qa.baespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordCollectionApplication.class, args);
+	}
+
+}
