74ed8b39b2e9bc2d5cd69ba05a956e7699f82529
RyanS186 <ryansharp186@gmail.com>
2022-04-25 18:04:27 +0100

    RS: Linked service to repo

--- src/main/java/com/qa/baespring/service/RecordsService.java
+++ src/main/java/com/qa/baespring/service/RecordsService.java
@@ -1,5 +1,18 @@
 package com.qa.baespring.service;
 
+import org.springframework.stereotype.Service;
+
+import com.qa.baespring.repo.RecordsRepo;
+
+@Service
 public class RecordsService {
 
+	//Link to repo
+	private RecordsRepo repo;
+	
+	public RecordsService(RecordsRepo repo) {
+		this.repo = repo;
+	}
+	
+	
 }
