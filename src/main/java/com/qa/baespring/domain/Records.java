3580f143d9a86b4a1f42ad9aa52c4b60ed33ebbc
RyanS186 <ryansharp186@gmail.com>
2022-04-25 17:03:00 +0100

    RS: Created table columns as private variables

--- src/main/java/com/qa/baespring/domain/Records.java
+++ src/main/java/com/qa/baespring/domain/Records.java
@@ -1,5 +1,30 @@
 package com.qa.baespring.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Records {
 
+	@Id // Setting column as the Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Setting the column to auto-increment
+	private long id;
+	
+	// Declaring private variables
+	
+	@Column(nullable = false)
+	private String albumName;
+	
+	@Column(nullable = false)
+	private String artistName;
+	
+	@Column
+	private String genre;
+	
+	@Column
+	private int releaseYear;
+	
 }
