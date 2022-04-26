6fd02f2966f23821b5d840337d9ef1c3d9c3584e
RyanS186 <ryansharp186@gmail.com>
2022-04-26 09:27:25 +0100

    RS: Linked controller to service

--- src/main/java/com/qa/baespring/controller/RecordsController.java
+++ src/main/java/com/qa/baespring/controller/RecordsController.java
@@ -1,5 +1,22 @@
 package com.qa.baespring.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.baespring.service.RecordsService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/Records")
 public class RecordsController {
 
+	// Link to service
+	private RecordsService service;
+	
+	public RecordsController (RecordsService service) {
+		this.service = service;
+	}
+	
+	
 }
