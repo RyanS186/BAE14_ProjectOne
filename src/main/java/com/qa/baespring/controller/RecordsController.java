f86f113332d83e37980c8bc2b8e5f4d773a40882
RyanS186 <ryansharp186@gmail.com>
2022-04-26 10:08:18 +0100

    RS: Added POST request, mapping and response entity

--- src/main/java/com/qa/baespring/controller/RecordsController.java
+++ src/main/java/com/qa/baespring/controller/RecordsController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,8 +36,16 @@ public class RecordsController {
 	}
 	
 	// Get by ID (Get one Record)
-	@GetMapping("/getById/{id}")
+	@GetMapping("/getById/{id}") // localhost:8080/records/getById
 	public ResponseEntity<Records> getById(@PathVariable long id) {
 		return new ResponseEntity<Records>(service.getByID(id), HttpStatus.OK);
 	}
+	
+	// POST
+	@PostMapping("/create") // localhost:8080/records/create
+	public ResponseEntity<Records> create(@RequestBody Records record) {
+		return new ResponseEntity<Records>(service.create(record), HttpStatus.CREATED);
+	}
+	
+	
 }
