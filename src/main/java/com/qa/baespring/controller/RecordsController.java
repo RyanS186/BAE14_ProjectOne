a936f49799a5c51f2287f0639c1fe6a5a9e9967f
RyanS186 <ryansharp186@gmail.com>
2022-04-26 10:12:41 +0100

    RS: Added PUT request, mapping and response entity

--- src/main/java/com/qa/baespring/controller/RecordsController.java
+++ src/main/java/com/qa/baespring/controller/RecordsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +46,12 @@ public class RecordsController {
 	@PostMapping("/create") // localhost:8080/records/create
 	public ResponseEntity<Records> create(@RequestBody Records record) {
 		return new ResponseEntity<Records>(service.create(record), HttpStatus.CREATED);
+	}
+	
+	// PUT
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Records> update(@PathVariable long id, @RequestBody Records record) {
+		return new ResponseEntity<Records>(service.update(id, record), HttpStatus.OK);
 	}
 	
 	
