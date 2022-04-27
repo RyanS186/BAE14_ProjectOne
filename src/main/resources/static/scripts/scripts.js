ecc82f08b21e51747bbd7665f0e787d39c2b26e6
RyanS186 <ryansharp186@gmail.com>
2022-04-27 18:44:00 +0100

    RS: Created a form contained within a modal for POST requests

--- src/main/resources/static/scripts/scripts.js
+++ src/main/resources/static/scripts/scripts.js
@@ -24,6 +24,12 @@ const getAll = () => {
         }).catch((err) => console.error(err))
 }
 
+// POST request
+const create = () => {
+    axios.post(`${address}/create`)
+}
+
+// Method to construct objects on the webpage to display objects from the API
 const printResult = (result) => {
 
     const linebreak = document.createElement("br");
@@ -49,11 +55,6 @@ const printResult = (result) => {
     release_year.setAttribute("class", "album-info");
     release_year.textContent = `${result.releaseYear}`;
 
-    // const artist_name = document.createElement("div");
-    // artist_name.setAttribute("class", "album-info");
-    // artist_name.textContent = `${result.artistName}`;
-
-    // TODO: Add spotify button
     const spotify = document.createElement("button");
     spotify.type = "button";
     spotify.setAttribute("class", "spotify-btn");
@@ -94,3 +95,30 @@ const printResult = (result) => {
     column_div.appendChild(card_div);
     results_div.appendChild(column_div);
 }
+
+// Modal
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("add-btn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
