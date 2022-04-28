3e5a351dddcac06563b0a5f7d5705e03aa67616e
RyanS186 <ryansharp186@gmail.com>
2022-04-28 10:37:18 +0100

    RS: Created RecordNotFound exception

--- src/main/java/com/qa/baespring/exceptions/RecordNotFoundException.java
+++ src/main/java/com/qa/baespring/exceptions/RecordNotFoundException.java
@@ -1,6 +1,17 @@
 package com.qa.baespring.exceptions;
 
+import javax.persistence.EntityNotFoundException;
 
-public class RecordNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code=HttpStatus.NOT_FOUND, reason="Record does not exist with that ID")
+public class RecordNotFoundException extends EntityNotFoundException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8451967977919402724L;
+	
 
 }
